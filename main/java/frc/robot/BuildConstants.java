6dc4d3342ee6908be0e35ee82bd5fe80d42cf591
Arjun <arjunkaushik20221@gmail.com>
2025-05-01 16:55:03 -0400

    AdvantageKit template

---
+++ main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_TalonFXSwerveTemplate";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-05-01 16:45:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1746132319346L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
